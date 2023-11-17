dbf12d13e996883425078f617889638807f3a9b1
waleery <walenda1997@gmail.com>
2023-11-17 14:43:13 +0100

    Getting server side props for heroBanner and products. Displaying heroBanner props

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,40 @@
-import React from 'react'
+import React from "react";
 
-import { Product, FooterBanner, HeroBanner } from '../components'
-import { client } from '@/lib/client'
+import { Product, FooterBanner, HeroBanner } from "../components";
+import { client } from "@/lib/client";
 
-const Home = () => {
-  return (
-    <>
-      <HeroBanner />
+const Home = ({ products, bannerData }) => {
+    return (
+        <>
+            <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
+            {console.log(bannerData)}
+            <div className="products-heading">
+                <h2>Best Selling Products</h2>
+                <p>Speakers of many variations</p>
+            </div>
 
-      <div className='products-heading'>
-        <h2>Best Selling Products</h2>
-        <p>Speakers of many variations</p>
-      </div>
+            <div className="products-container">
+                {products?.map((product) => product.name)}
+            </div>
+            <FooterBanner />
+        </>
+    );
+};
 
-      <div className='products-container'>
-        {['Product 1', 'Product 2'].map((product) => product)}
-      </div>
-      <FooterBanner />
-    </>
-  )
-}
+export const getServerSideProps = async () => {
+    //grab all the products from sanity dashboard
+    const queryProducts = '*[_type == "product"]';
 
-export default Home
+    const productsData = await client.fetch(queryProducts);
+
+    //grab all the banner data from sanity dashboard
+    const queryBanner = '*[_type == "banner"]';
+
+    const bannerData = await client.fetch(queryBanner);
+
+    return {
+        props: { productsData, bannerData },
+    };
+};
+
+export default Home;
