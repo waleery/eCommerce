f6837590d3da97edb35efa0a34c7c444f9e61644
waleery <walenda1997@gmail.com>
2023-11-17 14:03:12 +0100

    Addes scss and heroBanner layout

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import React from 'react'
 
+import { Product, FooterBanner, HeroBanner } from '../components'
+
 const Home = () => {
   return (
     <>
-      HeroBanner
+      <HeroBanner />
 
       <div className='products-heading'>
         <h2>Best Selling Products</h2>
@@ -13,7 +15,7 @@ const Home = () => {
       <div className='products-container'>
         {['Product 1', 'Product 2'].map((product) => product)}
       </div>
-      Footer
+      <FooterBanner />
     </>
   )
 }
